72f981cb0ab315d9eda83c309d1e53a3be2dc3ea
kbriggs <kabriggs991@gmail.com>
2017-01-31 17:14:50 -0500

    Checkbox

--- app/src/main/java/edu/cofc/buildingblocks/MainActivity.java
+++ app/src/main/java/edu/cofc/buildingblocks/MainActivity.java
@@ -2,50 +2,28 @@ package edu.cofc.buildingblocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputFilter;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText text_filtered;
+    CheckBox checkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] COLORS = {"red", "green", "orange", "blue", "purple", "black", "yellow", "cyan", "magenta"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COLORS);
-
-        AutoCompleteTextView text = (AutoCompleteTextView) findViewById(R.id.AutoCompleteTextView01);
-        text.setAdapter(adapter);
-
-        final Spinner spin = (Spinner) findViewById(R.id.Spinner01);
-
-
-        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        checkButton = (CheckBox)findViewById(R.id.checkbox);
+        checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                Toast.makeText(parent.getContext(),
-                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
-                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onClick(View v) {
+                CheckBox cb = (CheckBox) findViewById(R.id.checkbox);
+                cb.setText(checkButton.isChecked() ?
+                    "This option is checked" :
+                    "This option is NOT checked");
             }
         });
-
 
     }
 
