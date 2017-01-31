d69826bd4f9bd8c18a73a5a08215e3c4806848cb
kbriggs <kabriggs991@gmail.com>
2017-01-30 20:44:46 -0500

    filters

--- app/src/main/java/edu/cofc/buildingblocks/MainActivity.java
+++ app/src/main/java/edu/cofc/buildingblocks/MainActivity.java
@@ -2,12 +2,26 @@ package edu.cofc.buildingblocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text_filtered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_filtered = (EditText) findViewById(R.id.input_filtered);
+        text_filtered.setFilters(new InputFilter[] {
+                new InputFilter.AllCaps(),
+
+        });
+
+
     }
+
+
 }
